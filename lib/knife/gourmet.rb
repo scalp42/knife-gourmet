8c8c15d3af237b1363d7ba137af154305e87a2ed
Anthony Scalisi <scalisi.a@gmail.com>
2013-10-20 12:39:52 -0700

    bundle init

---
+++ lib/knife/gourmet.rb
@@ -0,0 +1,7 @@
+require "knife/gourmet/version"
+
+module Knife
+  module Gourmet
+    # Your code goes here...
+  end
+end
---
+++ lib/knife/gourmet/version.rb
@@ -0,0 +1,5 @@
+module Knife
+  module Gourmet
+    VERSION = "0.0.1"
+  end
+end
